bf83e57e0c25dfe195874ca9fc3e381d64125709
Juan Pablo Enrici <jpenrici@gmail.com>
2023-08-30 15:45:37 -0300

    Update.

--- SVG_Tree_Top-View_Generator_wxWidgets/svg.h
+++ SVG_Tree_Top-View_Generator_wxWidgets/svg.h
@@ -1,44 +1,60 @@
 #pragma once
 
+#include <cmath>
 #include <ctime>
 #include <fstream>
 #include <iostream>
-#include <math.h>
 #include <random>
 #include <string>
+#include <utility>
 #include <vector>
 
 constexpr auto PI = 3.1415926;
 
-inline double Rad(double angle) {
+inline auto Rad(const double &angle) -> double
+{
     return angle * PI / 180;
 }
 
-inline double Cos(double value, double radius, int angle)
+inline auto Cos(const double &value, const double &radius, const int &angle) -> double
 {
-    return value + radius * cos(Rad(angle));
+    return value + radius * std::cos(Rad(angle));
 }
 
-inline double Sin(double value, double radius, int angle)
+inline auto Sin(const double &value, const double &radius, const double &angle) -> double
 {
-    return value + radius * sin(Rad(angle));
+    return value + radius * std::sin(Rad(angle));
 }
 
-inline double Distance(double x0, double y0, double x1, double y1)
+inline auto Distance(const double &x0, const double &y0, const double &x1, const double &y1) -> double
 {
-    return sqrt((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1));
+    return std::sqrt((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1));
 }
 
-inline int LineAngle(double x0, double y0, double x1, double y1)
+inline auto LineAngle(const double &x0, const double &y0, const double &x1, const double &y1) -> int
 {
-    int angle = atan((y1 - y0) / (x1 - x0)) * 180.0 / PI;
-    if (x0 >  x1 && y0 == y1) { angle = 0; }
-    if (x0 == x1 && y0 <  y1) { angle = 90; }
-    if (x0 <  x1 && y0 == y1) { angle = 180; }
-    if (x0 == x1 && y0 <  y1) { angle = 270; }
-    if (x0 <  x1 && y0 >  y1) { angle += 180; }
-    if (x0 <  x1 && y0 <  y1) { angle += 180; }
-    if (x0 >  x1 && y0 <  y1) { angle += 360; }
+    int angle = static_cast<int>(std::atan((y1 - y0) / (x1 - x0)) * 180.0 / PI);
+    if (x0 >  x1 && y0 == y1) {
+        angle = 0;
+    }
+    if (x0 == x1 && y0 <  y1) {
+        angle = 90;
+    }
+    if (x0 <  x1 && y0 == y1) {
+        angle = 180;
+    }
+    if (x0 == x1 && y0 <  y1) {
+        angle = 270;
+    }
+    if (x0 <  x1 && y0 >  y1) {
+        angle += 180;
+    }
+    if (x0 <  x1 && y0 <  y1) {
+        angle += 180;
+    }
+    if (x0 >  x1 && y0 <  y1) {
+        angle += 360;
+    }
     return angle;
 }
 
@@ -49,12 +65,12 @@ public:
     struct Metadata {
         std::string creator = "SVG tree image in top view created automatically by algorithm in C++.";
         std::string title = "SVG Tree Top View";
-        std::string publisherAgentTitle = "";
-        std::string date = "";
+        std::string publisherAgentTitle;
+        std::string date;
 
-        Metadata(){};
+        Metadata() = default;
         Metadata(std::string creator, std::string title, std::string publisher)
-            : creator(creator), title(title), publisherAgentTitle(publisher) {}
+            : creator(std::move(creator)), title(std::move(title)), publisherAgentTitle(std::move(publisher)) {}
     };
 
     struct Point {
@@ -62,8 +78,12 @@ public:
         double x = 0.0;
         double y = 0.0;
 
-        Point(double x, double y) : x(x), y(y) {};
-        std::string toStr() { return std::to_string(x) + "," + std::to_string(y); }
+        Point(const double &x, const double &y) : x(x), y(y) {};
+
+        [[nodiscard]] auto toStr() const -> std::string
+        {
+            return std::to_string(x) + "," + std::to_string(y);
+        }
     };
 
     struct Shape {
@@ -73,19 +93,24 @@ public:
         std::vector<Point> points;
 
         Shape(std::string name,  std::string fill, std::string stroke, double strokeWidth)
-            : name(name), fill(fill), stroke(stroke), strokeWidth(strokeWidth), points({}) {}
-        Shape(std::string name,  std::string fill, std::string stroke, double strokeWidth, std::vector<Point> points)
-            : name(name), fill(fill), stroke(stroke), strokeWidth(strokeWidth), points(points) {}
+            : name(std::move(name)), fill(std::move(fill)), stroke(std::move(stroke)), strokeWidth(strokeWidth),
+              points({}) {}
+        Shape(std::string name,  std::string fill, std::string stroke, double strokeWidth,
+              std::vector<Point> points)
+            : name(std::move(name)), fill(std::move(fill)), stroke(std::move(stroke)), strokeWidth(strokeWidth),
+              points(std::move(points)) {}
     };
 
-    static const std::string RGB2HEX(unsigned R, unsigned G, unsigned B) {
+    static auto RGB2HEX(const unsigned &R, const unsigned &G, const unsigned &B)  -> std::string
+    {
         auto int2hex = [](unsigned value) {
             std::string digits = "0123456789ABCDEF";
-            std::string result = "";
+            std::string result;
             if (value < 16) {
                 result.push_back('0');
                 result.push_back(digits[value % 16]);
-            } else {
+            }
+            else {
                 while (value != 0) {
                     result = digits[value % 16] + result;
                     value /= 16;
@@ -97,141 +122,172 @@ public:
         return "#" + int2hex(R) + int2hex(G) + int2hex(B);
     }
 
-    static const std::string svg(int width, int height, const std::string& figure,
-                                 Metadata metadata)
+    static auto svg(const int &width, const int &height, const std::string &figure,
+                    Metadata metadata) -> std::string
     {
-        std::string now = "";
+        std::string now;
         try {
             std::time_t t = std::time(nullptr);
             std::tm *const pTm = std::localtime(&t);
             now = std::to_string(1900 + + pTm->tm_year);
-        } catch (...) {
-           // pass
+        }
+        catch (...) {
+            // pass
         }
 
-        metadata.date = metadata.date == "" ? now : metadata.date;
+        metadata.date = metadata.date.empty() ? now : metadata.date;
 
         return {
             "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n"
             "<svg\n"
-            "   xmlns:dc=\"http://purl.org/dc/elements/1.1/\"\n"
-            "   xmlns:cc=\"http://creativecommons.org/ns#\"\n"
-            "   xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n"
-            "   xmlns:svg=\"http://www.w3.org/2000/svg\"\n"
-            "   xmlns=\"http://www.w3.org/2000/svg\"\n"
-            "   xmlns:xlink=\"http://www.w3.org/1999/xlink\"\n"
-            "   width=\"" + std::to_string(width) + "\"\n"
-            "   height=\"" + std::to_string(height) + "\"\n"
-            "   viewBox= \"0 0 " + std::to_string(width) + " " + std::to_string(height) + "\"\n"
-            "   version=\"1.1\"\n"
-            "   id=\"svg8\">\n"
-            "  <title\n"
-            "     id=\"title1\">" + metadata.title + "</title>\n"
-            "  <defs\n"
-            "     id=\"defs1\" />\n"
-            "  <metadata\n"
-            "     id=\"metadata1\">\n"
-            "    <rdf:RDF>\n"
-            "      <cc:Work\n"
-            "         rdf:about=\"\">\n"
-            "        <dc:format>image/svg+xml</dc:format>\n"
-            "        <dc:type\n"
-            "           rdf:resource=\"http://purl.org/dc/dcmitype/StillImage\" />\n"
-            "        <dc:title>" + metadata.title + "</dc:title>\n"
-            "        <dc:date>" + metadata.date + "</dc:date>\n"
-            "        <dc:publisher>\n"
-            "          <cc:Agent>\n"
-            "            <dc:title>" + metadata.publisherAgentTitle + "</dc:title>\n"
-            "          </cc:Agent>\n"
-            "        </dc:publisher>\n"
-            "        <dc:subject>\n"
-            "          <rdf:Bag>\n"
-            "            <rdf:li>tree</rdf:li>\n"
-            "            <rdf:li>plant</rdf:li>\n"
-            "            <rdf:li>nature</rdf:li>\n"
-            "            <rdf:li>landscaping</rdf:li>\n"
-            "          </rdf:Bag>\n"
-            "        </dc:subject>\n"
-            "        <dc:creator>\n"
-            "          <cc:Agent>\n"
-            "            <dc:title>" + metadata.creator + "</dc:title>\n"
-            "          </cc:Agent>\n"
-            "        </dc:creator>\n"
-            "        <cc:license\n"
-            "           rdf:resource=\"http://creativecommons.org/publicdomain/zero/1.0/\" />\n"
-            "        <dc:description>SVG tree image in top view created automatically by algorithm in C++. Developer: https://github.com/jpenrici</dc:description>\n"
-            "      </cc:Work>\n"
-            "      <cc:License\n"
-            "         rdf:about=\"http://creativecommons.org/publicdomain/zero/1.0/\">\n"
-            "        <cc:permits\n"
-            "           rdf:resource=\"http://creativecommons.org/ns#Reproduction\" />\n"
-            "        <cc:permits\n"
-            "           rdf:resource=\"http://creativecommons.org/ns#Distribution\" />\n"
-            "        <cc:permits\n"
-            "           rdf:resource=\"http://creativecommons.org/ns#DerivativeWorks\" />\n"
-            "      </cc:License>\n"
-            "    </rdf:RDF>\n"
-            "  </metadata>\n"
-            "  <!--      Created in C++ algorithm       -->\n"
-            "  <!-- Attention: do not modify this code. -->\n"
-            "\n"
-            "" + figure + ""
-            "\n"
+            "xmlns:dc=\"http://purl.org/dc/elements/1.1/\"\n"
+            "xmlns:cc=\"http://creativecommons.org/ns#\"\n"
+            "xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n"
+            "xmlns:svg=\"http://www.w3.org/2000/svg\"\n"
+            "xmlns=\"http://www.w3.org/2000/svg\"\n"
+            "xmlns:xlink=\"http://www.w3.org/1999/xlink\"\n"
+            "width=\"" + std::to_string(width) + "\"\n"
+            "height=\"" + std::to_string(height) + "\"\n"
+            "viewBox= \"0 0 " + std::to_string(width) + " " + std::to_string(height) + "\"\n"
+            "version=\"1.1\"\n"
+            "id=\"svg8\">\n"
+            "<title\n"
+            "id=\"title1\">" + metadata.title + "</title>\n"
+            "<defs\n"
+            "id=\"defs1\" />\n"
+            "<metadata\n"
+            "id=\"metadata1\">\n"
+            "<rdf:RDF>\n"
+            "<cc:Work\n"
+            "rdf:about=\"\">\n"
+            "<dc:format>image/svg+xml</dc:format>\n"
+            "<dc:type\n"
+            "rdf:resource=\"http://purl.org/dc/dcmitype/StillImage\" />\n"
+            "<dc:title>" + metadata.title + "</dc:title>\n"
+            "<dc:date>" + metadata.date + "</dc:date>\n"
+            "<dc:publisher>\n"
+            "<cc:Agent>\n"
+            "<dc:title>" + metadata.publisherAgentTitle + "</dc:title>\n"
+            "</cc:Agent>\n"
+            "</dc:publisher>\n"
+            "<dc:subject>\n"
+            "<rdf:Bag>\n"
+            "<rdf:li>tree</rdf:li>\n"
+            "<rdf:li>plant</rdf:li>\n"
+            "<rdf:li>nature</rdf:li>\n"
+            "<rdf:li>landscaping</rdf:li>\n"
+            "</rdf:Bag>\n"
+            "</dc:subject>\n"
+            "<dc:creator>\n"
+            "<cc:Agent>\n"
+            "<dc:title>" + metadata.creator + "</dc:title>\n"
+            "</cc:Agent>\n"
+            "</dc:creator>\n"
+            "<cc:license\n"
+            "rdf:resource=\"http://creativecommons.org/publicdomain/zero/1.0/\" />\n"
+            "<dc:description>SVG tree image in top view created automatically by algorithm in C++."
+            "Developer: https://github.com/jpenrici</dc:description>\n"
+            "</cc:Work>\n"
+            "<cc:License\n"
+            "rdf:about=\"http://creativecommons.org/publicdomain/zero/1.0/\">\n"
+            "<cc:permits\n"
+            "rdf:resource=\"http://creativecommons.org/ns#Reproduction\" />\n"
+            "<cc:permits\n"
+            "rdf:resource=\"http://creativecommons.org/ns#Distribution\" />\n"
+            "<cc:permits\n"
+            "rdf:resource=\"http://creativecommons.org/ns#DerivativeWorks\" />\n"
+            "</cc:License>\n"
+            "</rdf:RDF>\n"
+            "</metadata>\n"
+            "<!--      Created in C++ algorithm       -->\n"
+            "<!-- Attention: do not modify this code. -->\n"
+            "\n" + figure + "\n"
             "  <!-- Attention: do not modify this code. -->\n"
             "</svg>"
         };
     }
 
-    static const std::string group(std::string id, std::string elements) {
+private:
+
+    static auto rtrimZeros(const std::string &str) -> std::string
+    {
+        auto right = str.size() - 1;
+        while (right >= 0) {
+            if (str[right] != '0') {
+                break;
+            }
+            right--;
+        }
+
+        return str.substr(0, 1 + right) + '0';
+    }
+
+    // Validates and formats entries.
+    static auto style(std::string name, std::string fill, std::string stroke, double strokeWidth,
+                      double fillOpacity, double strokeOpacity) -> std::string
+    {
+        name   = name.empty() ? "Shape" : name;
+        fill   = fill.empty() ? "#FFFFFF" : fill;
+        stroke = stroke.empty() ? "#000000" : stroke;
+        fillOpacity   = fillOpacity < 0 ? 0 : std::min(fillOpacity / 255, 1.0);
+        strokeOpacity = strokeOpacity < 0 ? 0 : std::min(strokeOpacity / 255, 1.0);
+
+        return {
+            "id=\"" + name + "\"\nstyle=\"" +
+            "opacity:" + rtrimZeros(std::to_string(fillOpacity)) + ";fill:" + fill +
+            ";stroke:" + stroke + ";stroke-width:" + rtrimZeros(std::to_string(strokeWidth)) +
+            ";stroke-opacity:" + rtrimZeros(std::to_string(strokeOpacity)) +
+            ";stroke-linejoin:round;stroke-linecap:round\"\n" };
+    }
+
+public:
+
+    static auto group(std::string id, const std::string &elements) -> std::string
+    {
         id = id.empty() ? "<g>\n" : "<g id=\"" + id + "\" >\n";
         return elements.empty() ? "" : id + elements + "</g>\n";
     }
 
-    static const std::string polyline(Shape shape) {
+    static auto polyline(Shape shape) -> std::string
+    {
 
         if (shape.points.empty()) {
             return "<!-- Empty -->\n";
         }
 
-        std::string values = "";
-        for (unsigned i = 0; i < shape.points.size(); i++) {
-            values += shape.points[i].toStr() + " ";
+        std::string values;
+        for (auto &point : shape.points) {
+            values += point.toStr() + " ";
         }
+
         shape.name = shape.name.empty() ? "polyline" : shape.name;
-        shape.stroke = shape.stroke.empty() ? "#000000" : shape.stroke;
-        std::string strokeWidth = std::to_string(shape.strokeWidth);
         return {
-            "     <polyline\n"
-            "        id=\"" + shape.name + "\"\n"
-            "        style=\"opacity:1.0;fill:none;stroke:" + shape.stroke + ";stroke-width:" + strokeWidth + ";stroke-opacity:1;stroke-linejoin:round;stroke-linecap:round\"\n"
-            "        points=\"" + values + "\" />\n"
+            "<polyline\n" + style(shape.name, "none", shape.stroke, shape.strokeWidth, 255, 255) +
+            "points=\"" + values + "\" />\n"
         };
     }
 
-    static const std::string polygon(Shape shape) {
+    static auto polygon(Shape shape) -> std::string
+    {
 
         if (shape.points.empty()) {
             return "<!-- Empty -->\n";
         }
 
-        std::string values = "";
+        std::string values;
         for (unsigned i = 0; i < shape.points.size() - 1; i++) {
             values += shape.points[i].toStr() + " L ";
         }
         values += shape.points[shape.points.size() - 1].toStr();
+
         shape.name = shape.name.empty() ? "polygon" : shape.name;
-        shape.fill = shape.fill.empty() ? "#FFFFFF" : shape.fill;
-        shape.stroke = shape.stroke.empty() ? "#000000" : shape.stroke;
-        std::string strokeWidth = std::to_string(shape.strokeWidth);
         return {
-            "     <path\n"
-            "        id=\"" + shape.name + "\"\n"
-            "        style=\"opacity:1.0;fill:" + shape.fill + ";stroke:" + shape.stroke + ";stroke-width:" + strokeWidth + ";stroke-opacity:1;stroke-linejoin:round;stroke-linecap:round\"\n"
-            "        d=\"M " + values + " Z\" />\n"
+            "<path\n" + style(shape.name, shape.fill, shape.stroke, shape.strokeWidth, 255, 255) +
+            "d=\"M " + values + " Z\" />\n"
         };
     }
 
-    static bool save(const std::string& text, std::string path = "")
+    static auto save(const std::string &text, std::string path = "") -> bool
     {
         if (path.empty()) {
             path = "svgOut.txt";
@@ -242,7 +298,7 @@ public:
             file << text;
             file.close();
         }
-        catch (const std::exception& e) {
+        catch (const std::exception &e) {
             std::cout << "Error handling file writing.\n";
             std::cerr << e.what() << "\n";
             return false;
